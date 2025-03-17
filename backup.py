fe684f6ffc658cf8c3e4f61cd82123af2479392b
Chup Yevhen <chup.yevhen@student.uzhnu.edu.ua>
2025-03-17 21:56:50 +0100

    Added log_message and ability to select files to restore

--- backup.py
+++ backup.py
@@ -5,6 +5,14 @@ import stat
 import zipfile
 from datetime import datetime
 
+LOG_FILE = "backup.log"
+
+def log_message(message):
+    time_stamp = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
+    with open(LOG_FILE, "a", encoding = "utf-8") as log:
+        log.write(f"[{time_stamp}] {message}\n")
+    print(message)
+
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -17,9 +25,9 @@ def create_backup():
         os.makedirs(DEST)
         
     timeTamp = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
-    print(timeTamp)
     backup_folder = os.path.join(DEST, f"backup_{timeTamp}")
     os.makedirs(backup_folder)
+    log_message(f"Створення резервної копії у {backup_folder}")
 
     for root, dirs, files in os.walk(SOURCE):
         relative_path = os.path.relpath(root, SOURCE)
@@ -32,29 +40,32 @@ def create_backup():
             file_source = os.path.join(root, file)
             file_dest = os.path.join(dest_path, file)
             shutil.copy2(file_source, file_dest)
-            print(f"Копіюємо файли із {file_source} у {file_dest}")
+            log_message(f"Копіювання: {file_source} у {file_dest}")
 
     zip_path = os.path.join(DEST, f"backup_{timeTamp}")
+    log_message("Архівація файлів")
     shutil.make_archive(zip_path, "zip", backup_folder)
-    safe_remove_folder(backup_folder)
+    log_message("Архівація файлів завершена")
+    remove_folder(backup_folder)
 
 def remove_readonly(func, path, exc_info):
     os.chmod(path, stat.S_IWRITE)
     func(path)
 
-def safe_remove_folder(folder_path):
+def remove_folder(folder_path):
     try:
         shutil.rmtree(folder_path, onerror=remove_readonly)
-        print(f"Папку {folder_path} успішно видалено")
+        log_message(f"Архів {folder_path} видалено")
     except Exception as e:
-        print(f"Помилка при видаленні {folder_path}: {e}")
+        log_message(f"Помилка при видаленні {folder_path}: {e}")
 
 def list_backups():
     backups = sorted(os.listdir(DEST), reverse = True)
     if not backups:
-        print("Резервні копії відсутні")
+        log_message("Резервні копії відсутні")
         return[]
     
+    log_message("Список резервних копій: ")
     for i, backup in enumerate(backups, 1):
         print(f"{i}. {backup}")
 
@@ -68,39 +79,83 @@ def restore_backup():
     try:
         choice = int(input("Введіть номер резервної копії: ")) - 1
         if choice < 0 or choice >= len(backups):
-            print("Такої копії не існує")
+            log_message("Такої копії не існує")
             return
     except ValueError:
-        print("Введіть число!")
+        log_message("Введіть число!")
         return
         
     backup_folder = os.path.join(DEST, backups[choice])
-
+    
     if backup_folder.endswith('.zip'):
         temp_extract_dir = os.path.join(DEST, f"temp_restore_{datetime.now().strftime('%Y-%m-%d_%H_%M_%S')}")
         os.makedirs(temp_extract_dir)
-        
+        log_message(f"Розпаковка {backup_folder} у {temp_extract_dir}")
+
         with zipfile.ZipFile(backup_folder, 'r') as zip_ref:
             zip_ref.extractall(temp_extract_dir)
         
+        log_message(f"Файли успішно розпаковані в {temp_extract_dir}")
+        extracted_files = os.listdir(temp_extract_dir)
+        if not extracted_files:
+            log_message("Помилка: після розпаковки файли не знайдено!")
+            return
+        else:
+            log_message(f"Знайдено файли: {extracted_files}")
+
         backup_folder = temp_extract_dir
     
-    print(f"Відновлення файлів з {backup_folder} у {SOURCE}")
+    log_message(f"Відновлення файлів з {backup_folder} у {SOURCE}")
+
+    files_list = []
 
     for root, dirs, files in os.walk(backup_folder):
-        relative_path = os.path.relpath(root, backup_folder)
-        restore_path = os.path.join(SOURCE, relative_path)
-
-        if not os.path.exists(restore_path):
-            os.makedirs(restore_path)
-        
         for file in files:
-            src_file = os.path.join(root, file)
-            dest_file = os.path.join(restore_path, file)
+            files_list.append(os.path.relpath(os.path.join(root, file), backup_folder))
+        
+    if not files_list:
+        log_message("У цій директорії відсутні файли")
+        return
+        
+    print("\nСписок файлів:")
+    for i, file in enumerate(files_list, 1):
+        print(f"{i}. {file}")
+        
+    selected_files = input("\nВведіть номери файлів для відновлення через кому, або напишіть all якщо хочете, щоб відновилася вся директорія\n")
+    
+    if selected_files.lower() == "all":
+        selected_files = files_list
 
+    else:
+        try:
+            selected_files = [int(num.strip()) for num in selected_files.split(",") if num.strip().isdigit()]
+            selected_files = [files_list[i - 1] for i in selected_files if 0 < i <= len(files_list)]
+        except ValueError:
+            log_message("Номера файлів введені некоректно")
+            return
+            
+        if not selected_files:
+            log_message("Жоден файл не вибрано")
+            return
+
+    for file in selected_files:
+        src_file = os.path.join(backup_folder, file)
+        dest_file = os.path.join(SOURCE, file)
+
+        dest_folder = os.path.dirname(dest_file)
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+
+        try:
             shutil.copy2(src_file, dest_file)
-    safe_remove_folder(temp_extract_dir)
-    print("Операція відновлення файлів завершена")
+            log_message(f"Відновлено: {file}")
+        except PermissionError:
+            log_message(f"Немає доступу: {file}")
+
+
+    remove_folder(temp_extract_dir)
+
+    log_message("Операція відновлення завершена")
 
 def main():
     print("Вітаю у програмі резервного копіювання файлів! Для початку виберіть дію:\n1. Створення резервної копії\n2. Відновлення файлів із уже існуючої резервної копії")
@@ -112,7 +167,7 @@ def main():
         elif choice == 2:
             restore_backup()
         else:
-            print("Введено неправильно число")
+            log_message("Введено неправильно число")
     except ValueError:
-        print("Введіть число!")
+        log_message("Введіть число!")
 main()
